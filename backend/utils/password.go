527a79b813dd4d11dcd04d73ff3e695635a00757
Oleg200606 <shev.oleg200606@mail.ru>
2025-11-17 22:48:50 +0300

    bd

--- backend/utils/password.go
+++ backend/utils/password.go
@@ -1,6 +1,6 @@
 package utils
 
-import "crypto/bcrypt"
+import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(password string) (string, error) {
     bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
